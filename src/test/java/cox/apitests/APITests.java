503092b55737957d08efa00f30965b6c3d2a63ce
auzundere <auzundere@3rdeyecam.com>
2020-11-06 11:49:46 -0600

    first commit

--- src/test/java/cox/apitests/APITests.java
+++ src/test/java/cox/apitests/APITests.java
@@ -1,20 +1,17 @@
 package cox.apitests;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import cox.base.TestBase;
 import cox.client.RestClient;
-import cox.data.Users;
 import cox.utilities.ConfigurationReader;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
-import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -43,101 +40,60 @@ public class APITests extends TestBase {
         URL = serviceURL + apiURL;
     }
 
-    /**
-     * User can Login with a valid username and password using "/login" endpoint of the API
-     */
-    @Test(groups = {"smoke", "login"}, priority = 1)
-    public void login(Method method) throws IOException {
+    @Test(groups = {"smoke"}, priority = 1)
+    public void getAllVehicles() throws IOException {
 
         restClient = new RestClient();
-        String username = "logger";
-        Map<String, String> headerMap = new HashMap<String, String>();
+        Map<String, String> headerMap = new HashMap<>();
         headerMap.put("Content-Type", "application/json");
-        headerMap.put("api-key", "special-key");
-        headerMap.put(username, "logger");
-        headerMap.put("password", "abc123");
-        closeableHttpResponse = restClient.get(URL + "/login", headerMap);
+        int datasetId = 12;
+        String path = "/" + datasetId + "/vehicles";
+        closeableHttpResponse = restClient.get(URL + path, headerMap);
         int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
         Assert.assertEquals(statusCode, RESPONSE_STATUS_CODE_200, "Status code was not 200");
         String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
-        Assert.assertTrue(responseString.contains("logged in user session:"));
+        String expectedResult = "{\n" +
+                "  \"vehicleIds\": [\n" +
+                "    0\n" +
+                "  ]\n" +
+                "}";
+        Assert.assertEquals(responseString, expectedResult, "The response body wa not the same, ");
         Header[] headersArray = closeableHttpResponse.getAllHeaders();
-        logHeaders();
+        LOGGER.info(Arrays.toString(headersArray));
     }
 
-    /**
-     * User can logout by using "/logout" end point of the API
-     */
-    @Test(groups = {"smoke", "logout"}, priority = 2)
-    public void logout(Method method) throws IOException {
+    @Test(groups = {"smoke"}, priority = 2)
+    public void getVehicle() throws IOException {
+
         restClient = new RestClient();
-        Map<String, String> headerMap = new HashMap<String, String>();
-
-        closeableHttpResponse = restClient.get(URL + "/logout", headerMap);
-
+        Map<String, String> headerMap = new HashMap<>();
+        headerMap.put("Content-Type", "application/json");
+        int datasetId = 12;
+        int vehicleId = 10;
+        String path = "/" + datasetId + "/vehicles/" + vehicleId;
+        closeableHttpResponse = restClient.get(URL + path, headerMap);
         int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
         Assert.assertEquals(statusCode, RESPONSE_STATUS_CODE_200, "Status code was not 200");
+        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+        String expectedResult = "{\n" +
+                "  \"vehicleId\": 10,\n" +
+                "  \"year\": 0,\n" +
+                "  \"make\": \"string\",\n" +
+                "  \"model\": \"string\",\n" +
+                "  \"dealerId\": 0\n" +
+                "}";
+        Assert.assertEquals(responseString, expectedResult, "The response body wa not the same, ");
         Header[] headersArray = closeableHttpResponse.getAllHeaders();
-        logHeaders();
+        LOGGER.info(Arrays.toString(headersArray));
     }
 
-    @Test(groups = {"smoke", "update"}, priority = 3)
-    public void updateAUser(Method method) throws IOException {
-        restClient = new RestClient();
-        String userName = "logger";
-        Map<String, String> headerMap = new HashMap<String, String>();
-        headerMap.put("accept", "application/json");
-        headerMap.put("Content-Type", "application/json");
-        headerMap.put("username", "logger");
-        headerMap.put("password", "abc123");
-
-        ObjectMapper mapper = new ObjectMapper();
-        Users users = new Users(1234, userName, "Resul", "Aslan", "abc@abc.com", "password", "1232344567", 1234567);
-
-        String usersJSonString = mapper.writeValueAsString(users);
-
-        closeableHttpResponse = restClient.put(URL + "/" + userName, usersJSonString, headerMap);
-
-        //1. get Status code
-        int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
-        Assert.assertEquals(statusCode, RESPONSE_STATUS_CODE_200);
-        logHeaders();
-    }
-
-    /**
-     * User can delete a user using by /{userName} end point
-     */
-    @Test(groups = {"smoke", "delete"}, priority = 4)
-    public void deleteAUser(Method method) throws IOException {
-        restClient = new RestClient();
-        String userName = "logger";
-        Map<String, String> headerMap = new HashMap<String, String>();
-        headerMap.put("accept", "application/json");
-        headerMap.put("Content-Type", "application/json");
-        headerMap.put("username", "logger");
-        headerMap.put("password", "abc123");
-
-        ObjectMapper mapper = new ObjectMapper();
-        closeableHttpResponse = restClient.delete(URL + "/" + userName);
-
-        //1. get Status code
-        int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
-        Assert.assertEquals(statusCode, RESPONSE_STATUS_CODE_200);
-        logHeaders();
-    }
 
     @AfterMethod
-    public void tearDown(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-        } else if (result.getStatus() == ITestResult.SKIP) {
-        }
+    public void tearDown() {
     }
 
     @AfterTest
     public void afterSuite() {
-    }
-
-    private void logHeaders(){
     }
 
 }
